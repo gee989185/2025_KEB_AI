e6677fdabd1ff11d6d0b9389c8097d36081d5108
gee989185 <gee020915@gmail.com>
2025-02-25 07:19:20 +0900

    v1.1 Assignment pandas & KNeighborRegressor

--- day11.py
+++ day11.py
@@ -1,14 +1,17 @@
+#Assignment: v0.7) v0.6의 최근접이웃모델과 같이 동작하는 커스텀 클래스를 설계하시오.
+
 #from statistics import LinearRegression
 
 # 선형회귀모델
-from sklearn.linear_model import LinearRegression
+#from sklearn.linear_model import LinearRegression
 # k-최근접 이웃 회귀 모델
 #from sklearn.neighbors import KNeighborsRegressor
 
 import pandas as pd
 import matplotlib.pyplot as plt
 
-from tglearn import LinearRegression
+#from tglearn import LinearRegression
+from tglearn import KNeighborsRegressor
 
 # 데이터를 다운로드하고 준비합니다.
 ls = pd.read_csv("https://github.com/ageron/data/raw/main/lifesat/lifesat.csv")
@@ -29,8 +32,8 @@ plt.axis([23_500, 62_500, 4, 9])
 plt.show()
 
 # 선형 모델을 선택합니다.(1. 선형회귀모델, 2. k-최근접 이웃 회귀모델)
-model = LinearRegression()
-#model = KNeighborsRegressor(n_neighbors=3)
+#model = LinearRegression()
+model = KNeighborsRegressor(n_neighbors=3)
 # 모델 훈련합니다.
 model.fit(X,y)
 
--- tglearn.py
+++ tglearn.py
@@ -1,5 +1,36 @@
 import numpy as np
 
+
+class KNeighborsRegressor:
+    def __init__(self,n_neighbors = 3):
+        self.n_neighbors = n_neighbors
+        self.X = None
+        self.y = None
+
+    def distance(self,X1,X2):
+        return np.sqrt(np.sum((X1 - X2)**2))
+
+
+    def fit(self,X,y):
+        self.X = X
+        self.y = y
+
+    def predict(self,X_test):
+        # 입력한 X값과 가장 가까운 distance를 가진 X값 추출(먼저 x값 저장 후 가장 가까운 n_neighbors 찾기)
+        # 그 X값의 Y값들을 평균 내기
+        y_predict =[]       # 예측값을 넣을 리스트(b)
+        for i in X_test:
+            distance_test =[]   # 입력 데이터와 다른나라GDP사이의 거리 리스트(a)
+            for j in self.X:
+                distance_test.append(self.distance(i,j))    # 저장(a)
+            distance_test = np.array(distance_test)
+            k_nearest = np.argsort(distance_test)[:self.n_neighbors]    #정렬 후 인덱스반환(n_neighbors까지)
+            k_nearest_y = self.y[k_nearest]
+            y_predict.append(np.mean(k_nearest_y))      # 저장(b)
+        return np.array(y_predict)
+
+
+
 class LinearRegression:
     def __init__(self):
         self.slope = None  # weight
