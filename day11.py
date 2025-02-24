aa880348f78856d9011455385b7be823cebb52e2
gee989185 <gee020915@gmail.com>
2025-02-24 14:12:26 +0900

    v0.7 pandas

--- day11.py
+++ day11.py
@@ -3,14 +3,43 @@
 
 import pandas as pd
 
-# s = pd.Series([1,2,3,4])
-s = pd.Series([1,2,3,4], index =['a','b','c','d'])
-print(s)
-print("-----------------")
+def square(n) -> int:
+    return  n*n
 
-s2 = pd.Series([99,100,98,91,92])
-s2_subset = s2[1:4]
-print(s2_subset)
-print('----------------------')
-s2_mean = s2.mean()
-print(s2_mean)
+df = pd.DataFrame(
+    [[4, 7, 10],
+     [5, 88, 11],
+     [16, 9, 12]],
+    index=[1, 2, 3],
+    columns=['a', 'b', 'c']
+)
+print(df)
+print('----------------')
+
+# df2 = pd.melt(df)
+# df2 = pd.melt(df).rename(columns={'variable':'var', 'value':'val'})
+df2 = (pd.melt(df)
+      .rename(columns={
+                'variable':'var',
+                 'value':'val'})
+      .query('val >= 200')
+).sort_values('val', ascending=False)
+# 위에 저거 안됨
+
+print(df2)
+# df3 = df.loc[1:2]
+# df3 = df.iloc[1:2]
+df3 = df.iloc[:,[0,2]]
+print(len(df3))
+print("--------------")
+print(df.apply(square))
+print('---------------')
+print(df.apply(lambda x: x*x))
+
+# melt
+# rename
+# query
+# sort.values
+# loc
+# iloc
+
