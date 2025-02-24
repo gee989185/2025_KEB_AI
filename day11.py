3c1c2a01ddfe1be2bea0052595dd8d280d80cb6d
gee989185 <gee020915@gmail.com>
2025-02-24 11:14:10 +0900

    v0.3

--- day11.py
+++ day11.py
@@ -1,24 +1,17 @@
 import numpy as np
 
-# v = np.array([1,3.9,-9,2])
-# print(v,v.ndim)
-
-# a = np.array([1,3,5,7])
-# print(a)
-# print(a,a.ndim)
-
-b = np.array([[1,2,3,4], [5,6,7,8], [9,10,11,12]])
-print(b, b.ndim, b.dtype)
-print()
-print(b, b.ndim, b.dtype, b.strides)
+# zeres(), ones(): 주어진 모양(shape)에 대해 모든 요소가 0 또는 1일 배열을 생성하는 함수
+# 3행 4열
+ones = np.ones((3,4))
+print(ones)
 print()
 
-c = np.array([[[1,2,"3"], [4,5,6]],[[7,8,9],[10,11,12]]])
-print(c,c.ndim, c.shape, c.dtype,c.strides)
+zeros = np.zeros((3,4))
+print(zeros)
+# zeros = np.zeros((3,4))
+zeros = np.zeros((3,4), dtype = np.int16)
 
-# ndim: 배열의 차원 수(1차원, 2차원, 3차원: 백터, 행렬, 텐서 등의 차원 확인)
-# dtype: 배열의 요소들의 데이터 타입
-# strides: 메모리 상에서 다음 요소로 이동하기 위한 바이트 수
-# ex) c >> (504, 252, 84): 첫 차원에서 다음 요소로 넘어갈때 504바이트를 이동해야함
-# shape: 배열의 각 차원의 크기를 튜플형태로
-# ex) [[1, 2, 3], [4, 5, 6]]의 shape은 (2, 3)입니다.
+# ndim
+# dtype
+# strides
+# shape
