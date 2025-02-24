891ac07a3132a47ba44eaa13116cbe897002faea
gee989185 <gee020915@gmail.com>
2025-02-24 14:57:45 +0900

    v0.9 pandas

--- day11.py
+++ day11.py
@@ -1,9 +1,9 @@
 #from statistics import LinearRegression
 
 # 선형회귀모델
-# from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression
 # k-최근접 이웃 회귀 모델
-from sklearn.neighbors import KNeighborsRegressor
+#from sklearn.neighbors import KNeighborsRegressor
 
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -27,12 +27,14 @@ plt.axis([23_500, 62_500, 4, 9])
 plt.show()
 
 # 선형 모델을 선택합니다.(1. 선형회귀모델, 2. k-최근접 이웃 회귀모델)
-# model = LinearRegression()
-model = KNeighborsRegressor(n_neighbors=3)
+model = LinearRegression()
+#model = KNeighborsRegressor(n_neighbors=3)
 # 모델 훈련합니다.
 model.fit(X,y)
 
 # 키프로스에 대해 예측을 만듭니다.
 print('------------')
-X_new = [[37_665.2]]            # 2020년 키프로스 1인당 GDP
+X_new = [[31_721.3]]            # 2020년 대한민국 1인당 GDP
 print(model.predict(X_new))     # 출력: [[6.30165767]]
+# k-최근접 이웃회귀모델: 5.7
+# 선형회귀모델: 5.89940454
